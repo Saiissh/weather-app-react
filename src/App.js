cc678343d8a7bcbce4d9e00895357c0fd6d5da9c
Saish Pawar <118446344+saishpawar@users.noreply.github.com>
2023-06-11 22:42:40 +0530

    done initial work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import axios from "axios";
+import { useState } from "react";
+import { BsCloudHaze } from "react-icons/bs";
 
 function App() {
+
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [spinner, setSpinner] = useState();
+  let city = "";
+
+
+  let api = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=25675717fe82d77523b70c705b7c296e&units=metric
+  `
+
+  function changeHandler(event) {
+
+    setLocation(event.target.value);
+  }
+
+  function cityHandler(event) {
+
+    setSpinner(true);
+    event.preventDefault();
+
+    getdata();
+    setSpinner(true);
+
+    setLocation('');
+  }
+  async function getdata() {
+    try {
+      let res = await fetch(api);
+      let data = await res.json();
+      setData(data);
+      setSpinner(false);
+      console.log(data.main);
+      console.log(data.weather[0].main);
+    }
+    catch {
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <div className="app">
+      <div className="container">
+
+        <form onSubmit={cityHandler}>
+          <input type="text" value={location} onChange={changeHandler} placeholder="Enter City" />
+        </form>
+        <div>
+          {
+            (spinner) ? <div class="spinner"></div> : <span></span>
+
+          }
+        </div>
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp " >
+            {
+              // if data.main available hai tabhi ye kro
+              data.main ? <h1>{Math.round(data.main.temp)} <span className="tempspan">°C</span></h1> : null
+            }
+          </div>
+
+
+          <div className="desc">
+            {
+              // if data.main available hai tabhi ye kro
+              data.weather ? <p>{data.weather[0].main}</p> : null
+            }
+          </div>
+        </div>
+
+        {
+          (data.main) ? <div className="bottom">
+            <div className="feels">
+
+              {
+                // if data.main available hai tabhi ye kro
+                data.main ? <p>{data.main.feels_like}</p> : null
+              }
+
+              <p className="bold">Feels like</p>
+            </div>
+            <div className="humidity">
+              {
+                // if data.main available hai tabhi ye kro
+                data.main ? <p>{data.main.humidity}</p> : null
+              }
+              <p>Humidity</p></div>
+            <div className="wind">
+              {
+                // if data.main available hai tabhi ye kro
+                data.wind ? <p>{data.wind.speed}<span > km/hr</span></p> : null
+              }
+              <p>Wind Speed</p></div>
+          </div> : null
+        }
+      </div>
     </div>
   );
 }
